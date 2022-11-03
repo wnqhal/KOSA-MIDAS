4e868e1483fd727904027485902f6511cc8dc1a7
Yeong joon <bluehome8626@naver.com>
2022-11-03 14:18:29 +0900

    chore: 프로젝트 세팅

---
+++ midas/src/router.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
